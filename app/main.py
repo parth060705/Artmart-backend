ff78e06461a822bfa2304674e1bae6da60a5015d
parth060705 <gharatparth513@gmail.com>
2025-06-23 09:59:49 +0530

    config

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api import routes
 from app.database import engine, Base
 from app.models import models
+# from config import settings
 
 Base.metadata.create_all(bind=engine)
 
@@ -20,7 +21,7 @@ app.include_router(routes.router)
 # ---------------------------------------------------------------------
 # to check and start the app
 
-# http://127.0.0.1:8000/docs
+# http://127.0.0.1:8000/docs     swagger ui
 #  uvicorn app.main:app --reload 
 
 
