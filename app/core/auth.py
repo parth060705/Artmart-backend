1ece7ad31b140ae8c5c91416fccec7acf607a121
parth060705 <parth.gharat@openlanesolutions.com>
2025-06-30 09:13:51 +0530

    expire token time extended to week

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timedelta
 from typing import Optional
-
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-
 from app.database import get_db
 from app.models.models import User
 from app.crud import crud
@@ -14,7 +12,7 @@ from app.crud import crud
 # JWT configuration
 SECRET_KEY = "your-secret-key"  # use env variable in production!
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 
 
 # OAuth2 scheme
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
