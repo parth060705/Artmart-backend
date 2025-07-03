7f4ae312753e1cc7dd468e7e6b8a7e91d8681e7c
parth060705 <parth.gharat@openlanesolutions.com>
2025-07-03 21:36:30 +0530

    middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api import routes
 from app.database import engine, Base
 from app.models import models
+from fastapi.middleware.cors import CORSMiddleware
 # from config import settings
 
 Base.metadata.create_all(bind=engine)
@@ -10,6 +11,14 @@ app = FastAPI(
     title="ARTMART API",
     description="Backend for the ARTMART art marketplace",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
