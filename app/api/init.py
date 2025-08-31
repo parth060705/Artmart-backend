c2e432b7c96c3d009397557d2fb7c68e8c381e7c
parth060705 <parth.gharat@openlanesolutions.com>
2025-08-31 09:51:10 +0530

    init

--- app/api/init.py
+++ app/api/init.py
@@ -1,13 +1,5 @@
-# from fastapi import APIRouter
-# from . import routes
+from fastapi import APIRouter
+from . import routes
 
-# router = APIRouter()
-# router.include_router(routes.router)
-
-
-import pkgutil
-import importlib
-
-# Dynamically import all modules inside the 'api' package
-for module_info in pkgutil.iter_modules(__path__):
-    importlib.import_module(f"{__name__}.{module_info.name}")
+router = APIRouter()
+router.include_router(routes.router)
